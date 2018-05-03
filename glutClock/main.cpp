5de6f3d12087fa8098da527ca3b5e161086b2b86
Ariel berkovich <arielberkovich1@gmail.com>
2018-05-03 20:24:59 +0300

    Refactored main to be more readable.

--- glutClock/main.cpp
+++ glutClock/main.cpp
@@ -1,21 +1,31 @@
 #include<stdio.h>
 #include<math.h>
+
+
 #include "GL/glut.h" 
 #include "Clock2D.h"
+#include "Displayer.h"
+#include "KeyboardHandler.h"
 
-#define LEFT -4.0
-#define RIGHT 4.0
-#define BOTTOM -4.0
-#define TOP 4.0
 
-void drawingCB(void);
-void reshapeFuncCB(int width, int height);
 
+
+void initializeClock();
+void initGlutWindow(int argc, char *argv[]);
+void assignCallbacks();
 
 //================================================================
 int main(int argc, char *argv[])
 {
 
+	initializeClock();
+	initGlutWindow(argc, argv);
+	assignCallbacks();
+	glutMainLoop();
+}
+//-----------------------------------------------
+void initializeClock()
+{
 	Color color1;
 	Color color2;
 	Color color3;
@@ -26,61 +36,20 @@ int main(int argc, char *argv[])
 
 
 	initClock(color1, color2, color3);
+}
+//----------------------------------------------
+void initGlutWindow(int argc, char *argv[])
+{
 	glutInit(&argc, argv);
 	glutInitWindowPosition(20, 20);
 	glutInitWindowSize(200, 200);
 	glutInitDisplayMode(GLUT_RGB);
-
-
 	glutCreateWindow("Clock");
+}
+//----------------------------------------------
+void assignCallbacks()
+{
 	glutReshapeFunc(reshapeFuncCB);
-	
-
 	glutDisplayFunc(drawingCB);
-
-	glutMainLoop();
+	glutKeyboardFunc(keyboardCB);
 }
-//----------------------------------------------------------------
-void drawingCB() // use as a callback to display
-{
-	int n;
-	GLenum er;
-	
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-	glClearColor(0.2f, 0.8f, 0.69f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	drawClock(17, 25,-2,-2);
-	drawClock(18, 35,2,-2);
-	drawClock(15, 5,2,2);
-	drawClock(16, 15,-2,2);
-
-	glFlush();
-
-	er = glGetError();  //get errors. 0 for no error, find the error codes in: https://www.opengl.org/wiki/OpenGL_Error
-	if (er) printf("error: %d\n", er);
-
-}
-//------------------------------------------------------------------
-void reshapeFuncCB(int width, int height)
-{
-	//vars:
-	float AR;
-	double left = LEFT, right = RIGHT, bottom = BOTTOM, top = TOP;
-	
-	//init:
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-    glViewport(0,0,width,height);
-    
-	//reshaping:
-	AR = (float)width/height;
-    if(AR>=1)  {left*=AR; right*=AR;}
-    else       { top/=AR; bottom/=AR;}
-    gluOrtho2D(left,right,bottom,top);
-}
-//-----------------------------------------------------------------
-
