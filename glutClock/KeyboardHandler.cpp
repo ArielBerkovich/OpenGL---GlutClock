b642f23f7a204b49abfb82818e4744ec723067b8
Ariel berkovich <arielberkovich1@gmail.com>
2018-05-03 20:24:39 +0300

    Seprated keyboards callbacks to their own files

---
+++ glutClock/KeyboardHandler.cpp
@@ -0,0 +1,30 @@
+#include "KeyboardHandler.h"
+#include "Clock2D.h"
+#include <stdio.h>
+
+//-------------------------------------------------
+void keyboardCB(unsigned char key, int x, int y)
+{
+
+	switch (key)
+	{
+
+
+
+	default: break;
+
+	}
+}
+void specialFuncsCB(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+
+
+
+	default: break;
+
+	}
+}
+//-------------------------------------------------
+
---
+++ glutClock/KeyboardHandler.h
@@ -0,0 +1,9 @@
+#ifndef KEYBOARDHANDLER_H
+#define KEYBOARDHANDLER_H
+
+
+void keyboardCB(unsigned char key, int x, int y);
+void specialFuncsCB(unsigned char key, int x, int y);
+
+
+#endif
